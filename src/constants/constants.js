17dbf5a6f4018095cffcdb674a1eda97a761a79e
Aneeq <aneequlwaseeq@gmail.com>
2023-10-15 20:41:39 +0530

    Update constants.js
    Card tile updates

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -17,7 +17,7 @@ export const projects = [
     visit: 'https://github.com/Aneeq2211/MemoriesApp',
     id: 7,
   },
-  {
+ /*  {
     title: 'Expense Tracker',
     description: "An Application used to manage user expenses, user can also add their expenses using voice command. It shows all the expense data Graphically.",
       image: '/images/Expense.png',
@@ -25,17 +25,17 @@ export const projects = [
     source: 'https://elegant-cray-a3790f.netlify.app/',
     visit: 'https://github.com/ayush200998/Expense-Tracker',
     id: 6,
-  },
+  }, */
   {
     title: 'Netflix Clone',
     description: "A Web app which shows list of movies to the user according to the Genre,It uses Email-password Authentication,The App is build using ReactJs and Firebase.",
       image: '/images/Netflix.png',
       tags: ['Firebase','React', 'Redux'],
     source: 'https://netflixclone-3c963.web.app/',
-    visit: 'https://github.com/ayush200998/Netflix-Clone',
+    visit: 'https://github.com/Aneeq2211/Netflix-Clone',
     id: 0,
   },
-  {
+  /* {
     title: 'Instagram Clone',
     description:"A Social media web app which allows users to post pictures, share their comments on other users pics.",
     image: '/images/Insta.png',
@@ -43,8 +43,8 @@ export const projects = [
     source: 'https://finstagram-91090.web.app/',
     visit: 'https://github.com/ayush200998/Finstagram',
     id: 1,
-  },
-  {
+  }, */
+  /* {
     title: 'Blogging Website',
     description: "A basic Blogging website where users can share their blogs, Users can also follow other users, A Global chat Application is also present where users can chat with each other. ",
       image: '/images/Blogging.png',
@@ -52,14 +52,14 @@ export const projects = [
     source: 'https://github.com/ayush200998/BloggingApp',
     visit: 'https://github.com/ayush200998/BloggingApp',
     id: 2,
-  },
+  }, */
   {
     title: 'Messenger App',
     description: "A global chat application in which users can chat with other users.",
     image: '/images/Messenger.png',
     tags: ['React', 'Firebase'],
     source: 'https://facebook-messenger-b37ea.web.app/',
-    visit: 'https://github.com/ayush200998/Messenger_app',
+    visit: 'https://github.com/Aneeq2211/Messenger_app',
     id: 3,
   },
   {
@@ -68,10 +68,10 @@ export const projects = [
     image: '/images/Hulu.png',
     tags: ['React', 'Firebase', 'NextJs', 'TMDB'],
     source: 'https://hulu-2-0-green.vercel.app/',
-    visit: 'https://github.com/ayush200998/Hulu_2.0',
+    visit: 'https://github.com/Aneeq2211/Hulu',
     id: 4,
   },
-  {
+  /* {
     title: 'Keeper',
     description: "A Todo list web app by which a user can keep a record of things he wants.",
     image: '/images/Keeper.png',
@@ -79,7 +79,7 @@ export const projects = [
     source: 'https://codesandbox.io/s/9uuwh',
     visit: 'https://codesandbox.io/s/9uuwh',
     id: 5,
-  },
+  }, */
 ];
 
 export const TimeLineData = [
@@ -87,5 +87,5 @@ export const TimeLineData = [
   { year: 2018, text: 'Worked on different things and learning new Programmings concepts and Programming Languages', },
   { year: 2019, text: 'Started JavaScript by making small Projects.', },
   { year: 2020, text: 'Started React and made a lots of Projects with it.This was the year which peaked my intrest in ML development', },
-  { year: 2021, text: 'Learning more and more frameworks to master the Front-End and also working on ML Ops', },
+  { year: 2021, text: 'Learning more and more frameworks to master the Front-End and also working on ML Ops. Same year started working as a Frontend Developer for Tata Consultancy Services.', },
 ];
